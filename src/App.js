c239a827f6ab961c9045e019c4924db4c732ed7f
Satyendra Singh More <satyendramore97@gmail.com>
2022-10-19 10:15:42 +0530

    all uis

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
+import Topbar from "./scenes/global/Topbar";
+import Sidebar from "./scenes/global/Sidebar";
+import Dashboard from "./scenes/dashboard";
+import Appointment from "./scenes/appoint";
+import Contacts from "./scenes/contacts";
+import MeetingInfo from "./scenes/infomeeting";
+import ProfileUpdate from "./scenes/updateprofile";
+import Line from "./scenes/line";
+import Calendar from "./scenes/calendar";
 
 function App() {
+  const [theme, colorMode] = useMode();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <Sidebar />
+          <main className="content">
+            <Topbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/appoint" element={<Appointment />} />
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/infomet" element={<MeetingInfo />} />
+              <Route path="/profileupdate" element={<ProfileUpdate />} />
+              <Route path="/line" element={<Line />} />
+              <Route path="/calendar" element={<Calendar />} />
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
